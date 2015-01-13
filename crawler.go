930922f3d05b45bac84a936d3bdec108ab547315
blackdev1l <blackdevil93@gmail.com>
2015-01-13 15:19:08 +0100

    rever

---
+++ crawler.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	gr "github.com/blackdev1l/go-crawler/graph"
+)
+
+func main() {
+	tree := gr.Node{}
+	tree.SetUrl("google.com")
+	url := tree.GetUrl()
+	fmt.Println(url)
+	tree.Add("yahoo.com")
+	tree.Add("mage.it")
+	tree.Add("leroy.com")
+}
